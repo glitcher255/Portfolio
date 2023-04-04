e0f032bb30a4c21968e66c75133831708229f7f8
Your Name <you@example.com>
2023-04-04 15:49:51 +0200

    t

--- home_pages/page2 COMPLETE/page2.js
+++ home_pages/page2 COMPLETE/page2.js
@@ -4,38 +4,38 @@ let cancel_interval = false
 
 //Auto rotates to 90 +- 3 ish degrees because of prespective being global
 let myInterval = setInterval(wait, 100)
-function wait () {
+function wait() {
     if (cancel_interval === false) {
         console.log(cancel_interval)
-    document.getElementsByClassName("div502")[z].style.transform = "rotateY(" + y + "deg)"
-    y -= 3.2
-    document.getElementsByClassName("div502")[z].style.visibility = "visible"
-    document.getElementsByClassName("div502")[z].style.animationPlayState = "running"
-    z += 1
-    if (z === 3) {
-        clearInterval(myInterval);
-        setTimeout(interval_over, 500)
+        document.getElementsByClassName("div502")[z].style.transform = "rotateY(" + y + "deg)"
+        y -= 3.2
+        document.getElementsByClassName("div502")[z].style.visibility = "visible"
+        document.getElementsByClassName("div502")[z].style.animationPlayState = "running"
+        z += 1
+        if (z === 3) {
+            clearInterval(myInterval);
+            setTimeout(interval_over, 500)
+        }
     }
-}
 }
 
 //reset rotate to 0 after finish - because of wait() func
-function interval_over () {
-for (let z = 0; z < document.getElementsByClassName("div502").length; z++) {
-    document.getElementsByClassName("div502")[z].style.transform = "rotateY(" + "0deg" + ")"
-}
+function interval_over() {
+    for (let z = 0; z < document.getElementsByClassName("div502").length; z++) {
+        document.getElementsByClassName("div502")[z].style.transform = "rotateY(" + "0deg" + ")"
+    }
 }
 
 //Resets rotate to 0 on hover - because of wait() func
 for (let x = 0; x < document.getElementsByClassName("div502").length; x++) {
-    document.getElementsByClassName("div502")[x].addEventListener("mouseover", function() {
+    document.getElementsByClassName("div502")[x].addEventListener("mouseover", function () {
         document.getElementsByClassName("div502")[x].style.transform = "rotateY(" + "0deg" + ")"
     })
 }
 
 const fade_animation = [
     {},
-    {opacity: "0"},
+    { opacity: "0" },
 ]
 
 const fade_timing = {
@@ -46,5 +46,6 @@ const fade_timing = {
 // const fade_animated = document.querySelector
 document.getElementById("div16").addEventListener('click', function () {
     for (let x = 0; x < document.getElementsByClassName("div502").length; x++) {
-    document.getElementsByClassName("div502")[x].animate(fade_animation, fade_timing)
-}})
+        document.getElementsByClassName("div502")[x].animate(fade_animation, fade_timing)
+    }
+})
